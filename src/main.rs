cc066494f1c5b69db50c7886cc1cd4e93596e740
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-01-24 19:13:29 +0000

    WIP got basic structure, need to fix bugs from cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let sum = 0;
+    // Read the filename from args
+    let filename = std::env::args().nth(1).ok_or("Expected filename")?;
+
+    // read the file
+    let file = std::fs::File::open(filename)?;
+    let reader = std::io::BufReader::new(file);
+    let contents = std::string::String::new();
+    for line in reader.lines() {
+        // parse line as a single number
+        let num = line.parse().ok_or("{num} can't be parsed as a number")?;
+        // sum up number
+        sum += num;
+    }
+
+    // return the sum
+    println!("Total of all lines is {sum}");
 }
