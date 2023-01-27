432f4950c47dadf10dd6e7a87695562c6555cc3c
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-01-27 19:55:49 +0000

    Fix some more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-fn main() {
-    let sum = 0;
+use std::io::BufRead;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut sum: i32 = 0;
     // Read the filename from args
     let filename = std::env::args().nth(1).ok_or("Expected filename")?;
 
     // read the file
     let file = std::fs::File::open(filename)?;
     let reader = std::io::BufReader::new(file);
-    let contents = std::string::String::new();
     for line in reader.lines() {
         // parse line as a single number
-        let num = line.parse().ok_or("{num} can't be parsed as a number")?;
+        //println!("{}", line?);
+        let num: i32 = line?.parse().unwrap();
         // sum up number
         sum += num;
     }
 
     // return the sum
     println!("Total of all lines is {sum}");
+    Ok(())
 }
