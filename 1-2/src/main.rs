b01dba2dc0bcb80eaa8bf2d450998177308bdcb9
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-02-07 19:11:16 +0000

    Use type parameters to collect into a Result

--- 1-2/src/main.rs
+++ 1-2/src/main.rs
@@ -5,8 +5,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let filename = std::env::args().nth(1).ok_or("Expected filename")?;
     let file = std::io::BufReader::new(std::fs::File::open(filename)?);
     // BufReader.bytes() returns an iterator of Results. First, handle Error, then filter.
-    let loaded: Result<std::vec::Vec<_>,_> = file.bytes().collect();
-    let prog = loaded?.into_iter().filter(|x| BRAINFUCK_CHARS.contains(x)).collect();
+    let prog = file.bytes()
+        .collect::<Result<std::vec::Vec<_>,_>>()?
+        .into_iter()
+        .filter(|x| BRAINFUCK_CHARS.contains(x))
+        .collect();
     println!("{}", std::string::String::from_utf8(prog)?);
     Ok(())
 }
