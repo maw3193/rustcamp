df9de65427f9fe6d7076ce13671c790565e5a483
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-02-08 11:04:06 +0000

    Rework homework 1-1 to use iterator methods instead of a for-loop

--- 1-1/src/main.rs
+++ 1-1/src/main.rs
@@ -1,20 +1,19 @@
 use std::io::BufRead;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut sum: i32 = 0;
-    // Read the filename from args
     let filename = std::env::args().nth(1).ok_or("Expected filename")?;
 
-    // read the file
     let file = std::fs::File::open(filename)?;
     let reader = std::io::BufReader::new(file);
-    for line in reader.lines() {
-        // parse line as a single number
-        let num: i32 = line?.parse()?;
-        // sum up number
-        sum += num;
-    }
 
-    // return the sum
+    let sum = reader.lines().map({
+        |line| match line {
+            Ok(text) => {
+                text.parse::<i32>()?
+            }
+            Err(e) => e
+        }
+    }).fold(0, |sum, i| sum + i);
+
     println!("Total of all lines is {sum}");
     Ok(())
 }
