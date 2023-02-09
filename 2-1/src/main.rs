18df8e7f30bf94adf9d7fb6dc2a0e9ef9c1d3928
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-02-09 19:53:47 +0000

    Add load_input_entries
    Rewrote try_from to return a boxed error, rather than faff with Results.

--- 2-1/src/main.rs
+++ 2-1/src/main.rs
@@ -1,4 +1,5 @@
 use std::convert::TryFrom;
+use std::io::BufRead;
 use std::vec::Vec;
 
 enum InputEntry {
@@ -7,22 +8,30 @@ enum InputEntry {
 }
 
 impl TryFrom<&str> for InputEntry {
-    fn try_from(value: &str) -> Result<Self, String> {
+    fn try_from(value: &str) -> Result<Self, Box<dyn std::error::Error>> {
         // parse string. split by colon, left is string, right is u32.
         let parts: Vec<&str> = value.split(":").collect();
         if parts.len() == 1 {
             Ok(InputEntry::NameOnly(parts[0].to_string()))
         } else if parts.len() == 2 {
-            match parts[1].parse() {
-                Ok(number) => Ok(InputEntry::NameAndNumber(parts[0].to_string(), number)),
-                Err(_) => Err(format!("Could not parse {} into u32", parts[1])),
-            }
+            Ok(InputEntry::NameAndNumber(parts[0].to_string(), parts[1].parse()?))
         } else {
-            Err(format!("{} was not split by colons into 1 or 2 parts", value))
+            Err(Box::from(format!("{} was not split by colons into 1 or 2 parts", value)))
         }
     }
 
-    type Error = String;
+    type Error = Box<dyn std::error::Error>;
+}
+
+fn load_input_entries(filename: &String) -> Result<Vec<InputEntry>, Box<dyn std::error::Error>> {
+    let file = std::io::BufReader::new(std::fs::File::open(filename)?);
+    // file.lines is a Result, because it may fail.
+    // InputEntry::try_from(str) is a Result because it may fail.
+    file.lines()
+        .collect::<Result<Vec<_>,_>>()?
+        .into_iter()
+        .map(|x| InputEntry::try_from(x.as_str()))
+        .collect::<Result<Vec<_>, _>>()
 }
 
 fn main() {
