101455f29a07b8b4365e99d8f77cf1b8eb758958
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-02-25 20:23:34 +0000

    Write a simple main

--- bft/src/main.rs
+++ bft/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use bft_interp::Machine;
+use bft_types::Program;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filename = std::env::args().nth(1).ok_or("Expected filename")?;
+    let prog = Program::from_file(filename.as_ref())?;
+    let machine: Machine<u8> = Machine::new(0, false);
+    Ok(())
 }
