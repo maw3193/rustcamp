98039415c3ff37855fcd7e2e5019455d378b1897
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-03-08 19:35:47 +0000

    bft: Load args using clap

--- bft/src/main.rs
+++ bft/src/main.rs
@@ -1,10 +1,12 @@
 use bft_interp::Machine;
 use bft_types::Program;
+use clap::Parser;
+mod cli;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let filename = std::env::args().nth(1).ok_or("Expected filename")?;
-    let prog = Program::from_file(&filename)?;
-    let machine: Machine<u8> = Machine::new(None, false);
+    let args = cli::Cli::parse();
+    let prog = Program::from_file(&args.program)?;
+    let machine: Machine<u8> = Machine::new(args.cells, args.extensible);
     machine.print_program(&prog);
     Ok(())
 }
