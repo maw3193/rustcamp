5d5dbb8307e004b9520f7e17e5b2fb50e0186abd
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-03-14 19:03:57 +0000

    Parse brackets to find bracket-matching errors

--- bft/src/main.rs
+++ bft/src/main.rs
@@ -7,6 +7,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = cli::Cli::parse();
     let prog = Program::from_file(&args.program)?;
     let machine: Machine<u8> = Machine::new(args.cells, args.extensible);
-    machine.print_program(&prog);
+    machine.validate(&prog)?;
     Ok(())
 }
