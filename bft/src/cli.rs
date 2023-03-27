e83c67870b74a7d14de013890d373fe0e03eaa17
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-03-27 19:14:50 +0100

    Return proper errors

--- bft/src/cli.rs
+++ bft/src/cli.rs
@@ -1,6 +1,8 @@
 use std::{num::NonZeroUsize, path::PathBuf};
-
 use clap::Parser;
+
+use bft_interp::Machine;
+use bft_types::Program;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -10,4 +12,12 @@ pub(crate) struct Cli {
     pub(crate) cells: Option<NonZeroUsize>,
     #[arg(short, long)]
     pub(crate) extensible: bool,
+}
+
+pub(crate) fn run_bft() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+    let prog = Program::from_file(&args.program)?;
+    let machine: Machine<u8> = Machine::new(args.cells, args.extensible);
+    machine.validate(&prog)?;
+    Ok(())
 }
