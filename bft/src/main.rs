15103ca90c9a19fbf8938ec18baa108f4c96bf21
Jonathan Maw <jonathan.maw@codethink.co.uk>
2023-04-04 19:05:01 +0100

    Post-review fixes
    Significantly, this includes:
    * Figuring out how to impolement Program::new without using attributes
      other than AsRef<Path>
    * Actually returning a non-zero exit code

--- bft/src/main.rs
+++ bft/src/main.rs
@@ -1,7 +1,9 @@
 mod cli;
+use std::process;
 
 fn main() {
     if let Err(e) = cli::run_bft() {
-        println!("{}: Error: {}", std::env::args().next().unwrap(), e,)
+        println!("{}: Error: {}", std::env::args().next().unwrap(), e,);
+        process::exit(1);
     }
 }
